524ce06ec83f210f589b23b6704104be9ad620c6
Kos Ivantsov <kos@truetranslation.org>
2024-09-11 11:56:15 +0200

    Improve utils_BibleGUI.groovy

--- tools/OmegaTscripts/utils_BibleGUI.groovy
+++ tools/OmegaTscripts/utils_BibleGUI.groovy
@@ -31,6 +31,7 @@ import java.io.FileWriter
 import java.io.IOException
 
 import javax.swing.*
+import javax.swing.ImageIcon
 import javax.swing.JDialog
 import javax.swing.JEditorPane
 
@@ -38,7 +39,6 @@ import org.omegat.core.Core
 import org.omegat.core.CoreEvents
 import org.omegat.core.data.ProjectProperties
 import org.omegat.core.events.IProjectEventListener
-import org.omegat.core.events.IProjectEventListener.PROJECT_CHANGE_TYPE
 import org.omegat.core.search.SearchMode
 import org.omegat.util.OStrings
 import org.omegat.util.Preferences
@@ -48,7 +48,7 @@ import org.omegat.util.gui.StaticUIUtils
 import org.omegat.util.gui.UIThreadsUtil
 
 @Field final int TRIGGER_KEY = KeyEvent.VK_F10
-@Field final String SCRIPT_NAME = "Show Bible GUI"
+SCRIPT_NAME = "Show Bible GUI"
 
 KeyListener createEditorKeyListener() {
     [
@@ -71,6 +71,21 @@ KeyListener createEditorKeyListener() {
         }
     ] as KeyAdapter
 }
+
+pngData = 'iVBORw0KGgoAAAANSUhEUgAAADAAAAAwCAMAAABg3Am1AAADAFBMVEVHcEw7OzstLy8jIyNVVUlISEcVFhVqamoDAwMoKSpNTU0AAAAJCQlxb2wkN0OEbUtDQ0NkZGQAAAAAAAI5RE1YWFRJSUkAAAAbGxs9Pj5fX2AiIiAsOERSUlIABBeTiHLkvXspKyo7QU0gIiIhIiL///9cXFwAAARBQ0Q+Pj4XFhM0NDQtLi4PERHsyoxMTExOTk4OLDtUU1KDeGQtKyk5PDw4ODdSUlIABiZUVFQyLytYTDwUGBpaWVprYEsAFi9OSD0AAQErLi5pWjwCChH//38DDA8QGB8sLCxGRkZbUUW4nm1XVVMVFhdJS0smJiQ2NjaRhGtNS0s+Pj5WVlZHR0c8PDxTUlBQUFBFRUUxLyoUFBEnJB9BQUDgwIZGTlJkWk4xNDRMS0lQT05aWVllZWNnZmZRUVFVVVVpaWlLS0tjYmJhYWD17klXV1ZfXl5NTUxHR0dFRENJSUlYWFdJSkvfo1P45cFcW1n63qkDHzNVV19VVFBCRk5bXV9ESFtPU11BQ1kuMUv+4q3215z43adsbGv55b3+36Hjp1P/5rb558T54K82O0X32qBkZGT54rT48Ub547g8OzpBQUH96L3qtGVJR0PjrV/quW7uxYQ2NDHrwHl7cVbmxIeTjl3+9kry6k/j3FJ7d1zt5EwkMD7pz5+ymXAsLi7iqFj7253z0pTmunTjsGpdXVdmY1Y4PU/b1FvHs5Df11LlwID426TcrWf23a7p4VYALD52WzYSKzrorlzHmVeiknGpiFkuNDrjnDnktWzDroetgklfVkUZJi9tamD/7MbpyYz/6r7DqXncv4mYjHU6P1lhXE1LTlPdm0JkY1+RhFi5sl7q1a2hmFy/uF+WkE0hJ02qpGLSzFitplVqZVBCPjpTU1PiqmXKu5z+7Mjr2LK1fjLe0bP+8M9LPi6Pazo4PkPcyKG0pYacfEuNfF3HoGWbh2Lct3m5q013dGXyzoy5l2Djy0/RrXD/8kXHk1DcmD/WkzcAB13HtZHkuID38GK4sUtYBs5hAAAAY3RSTlMAAQMFA/sP+hz96Rck/v3+4voa+/375S0g3vqM+3b9/v5V/lyZAfo1+tVC5cBKvj+V+tP+0yO3Xv7t6f1z+/7++ZFpIsMCUN599v3Pyv1ztaz9gzEvw0jf/J30Zaqr0/j+YvGtdVq9AAAD2ElEQVR4Xu2UZXcbRxhGd2VJlmzJims7bWzXlNRhhgYahjIzw+6KmZnMzMzMGGZmZmaGMnNnZLVHqiWd/IC8n+89zzPvzC7y3zwalEQik9CH50kDANnVQn3y1OGLxtDQf8McFuqLDxyNi7HnR7/23HiGnwN4ikTyyQ+TxM9VKsVcjqpw2IyI4SOpMMEXL4zHAc4N476HYRhfXjB23IgxiJcMP+SxIcJwLa7kKk1n7Sopn0KhyMDQGcDwyNOGCMIlQgne3Xv87zOHVVKVQiaTj6K0Pg4Ez3xOuNBwyMa1VTXWrumRDQiUgoXe+KFEsMBa9e2Zixfr89Y0xBlVQJBTCpeAU3vm2cEC8+4ff65tbBSJLtjtdqkCJBTSFyNkT/wTQ3nBAoGZeer0chGYuvrzp7KkMhmF/iKCeuH9cwRCodn6+wmRqK+vrvavGiBgM1smIEs98QE8fzYQJITtgijvvMXacK7mLKbA9KyOCeCuPfDJ/mwoEJaaurx6yRGjyd6LqbCsOTHff0Ijg1Lu/HTA86BAWBsawYIsystGI4cvVdB/Kq8+sOBZ96smIwzIQ4Ew9NTn1dXYtJc5HPAusJk/lK+orp63K8JVQJHps5hMHhQIwbHltaJzh7q5UJAa9Q8qY1bs3TT/A4a78LY4OIfJTGYTOZLvTvSdtnWLHYIxaF1ZZ0z5fn3Fh4ifK/+W/OXWnVm9BnYyjzBbDAYtDgWMY7q9qqyz8psFrR+5XRwJeVoeNa/jQEuVvCkUZ+8+JsGVjgTOro1l2zorWfrZVAC5Ck/KQ75MTU3QNfcfPLylSaUFrxUIYv2O4q5tlZvnV4wHhdyFkpCv2xOzkxLS01ffuz+1RN/Ex3F87Z0NpV1lX00ugBsaJBwHwsqkpMzcon1F169u6qGvX7surbS0eNWcLdMcT9X9DPyQqkQo6DIzM3Pzi/Zc09xsuaFuaystZq2vGPTtkJGJWNTODqeRm5ufkqLJuFW+VZ3WVrx58qVnnAdwXSt1VkVUdWp7dvZKHUzIT9FoMrqOqtUbNkYWvurs4x4xSTJq6v697am6pNXOCM2Vo+o09Qt3ZwfCQoMjphAGS/zr25t1CQkwwymwgi4tg4U8Ga+8pBVaQ+dGb28Gu4Wlun5N2xFUMsLL/wjWfPONKVrzkbiw6P6T6fuKMv7c+kuoYpofiPdskICDTpz0rhY3RYZF/3FyT+Jvn5kKRjoLeXGA6PfO+wFKblxk2MF+VqjCuVHvChk61NiPvxBzsoI+xca59PHtBH4+g4Lxx9JcT+yrGnz7jNhYqifexwqQh+cdMAlKcB7NP8/ng7eXQcsKAAAAAElFTkSuQmCC'
+tmpDir = System.getProperty("java.io.tmpdir")
+outImg = { base64, name ->
+    outFile = new File(tmpDir + File.separator + name + ".png")
+    System.setProperty("omegat.bible.icon", outFile.toString())
+    if (! outFile.exists()){
+        outFile.createNewFile()
+    }
+    imageOutFile = new FileOutputStream(outFile)
+    imageByteArray = Base64.getDecoder().decode(base64)
+    imageOutFile.write(imageByteArray)
+}
+outImg(pngData, "mybible")
+
 
 class BibleGUIKeyListener extends KeyAdapter {
     String name
@@ -135,6 +150,10 @@ class OutputWindow {
             dialog.setTitle("Bible")
             dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE)
 
+            // Set icon
+            ImageIcon icon = new ImageIcon(System.getProperty("omegat.bible.icon"))
+            dialog.setIconImage(icon.image)
+
             // Create a JEditorPane and set it to display HTML
             JEditorPane editorPane = new JEditorPane("text/html", outputText)
             editorPane.setEditable(false)
@@ -184,6 +203,7 @@ class OutputWindow {
             // dialog.setSize(new Dimension(geometry['width'], geometry['height']))
             dialog.setLocation(new Point(geometry['x'] ?: 0, geometry['y'] ?: 0))
             dialog.pack()
+            dialog.setAlwaysOnTop(true)
             dialog.setVisible(true)
             resetModifiers()
         }
@@ -441,9 +461,15 @@ class BibleWindowController implements IProjectEventListener {
                     installKeyListener()
                 } as Runnable
                 SwingUtilities.invokeLater doRun
+                System.setProperty("omegat.bible.status", "Script is activated for the current session.")
                 break
             case PROJECT_CHANGE_TYPE.CLOSE:
                 uninstallKeyListener()
+                System.setProperty("omegat.bible.status", "Script is deactivated for the current session.")
+                break
+            case PROJECT_CHANGE_TYPE.SAVE:
+                System.setProperty("omegat.bible.status", "Project saved...")
+
                 break
         }
     }
@@ -468,5 +494,5 @@ if (! isAvailable()) {
     return "$SCRIPT_NAME >> This script cannot run in OmegaT versions earlier than 4."
 }
 
-CoreEvents.registerProjectChangeListener new BibleWindowController(createEditorKeyListener())
-"${SCRIPT_NAME} is activated for the current session."
+CoreEvents.registerProjectChangeListener(new BibleWindowController(createEditorKeyListener()))
+System.getProperty("omegat.bible.status") ? ((System.getProperty("omegat.bible.status")) ? "Project saved..." : "$SCRIPT_NAME >> ${System.getProperty("omegat.bible.status")}") : ""
